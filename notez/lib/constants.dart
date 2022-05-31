62de259dd9049adbde2c0549e2bda18e7b9ea428
linm1 <linm1@bxscience.edu>
2022-05-30 21:02:23 -0400

    homepage day and night icons

---
+++ notez/lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+ const ImageIcon sun = ImageIcon(AssetImage('assets/button_icons/ssun.png'), size: 200, );
+ const ImageIcon moon = ImageIcon(AssetImage('assets/button_icons/moon.png'), size: 100 );
+ 
--- notez/lib/screens/home_screen.dart
+++ notez/lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_switch/flutter_switch.dart';
-import 'package:notez/constants.dart' as Constants;
+import 'package:notez/constants.dart'as constants;
 import '../main.dart';
 
 // Home Screen
@@ -27,30 +27,18 @@ class _myHomePageState extends State<HomeScreen>{
           children: [
             FlutterSwitch(
                width: 100.0,
-                      height: 55.0,
-                      toggleSize: 45.0,
-                      borderRadius: 30.0,
-                      padding: 2.0,
-                      activeToggleColor: Color(0xFF6E40C9),
-                      inactiveToggleColor: Color(0xFF2F363D),
-                      activeSwitchBorder: Border.all(
-                        color: Color(0xFF3C1E70),
-                        width: 6.0,
-                      ),
-                      inactiveSwitchBorder: Border.all(
-                        color: Color(0xFFD1D5DA),
-                        width: 6.0,
-                      ),
-                      activeColor: Color(0xFF271052),
-                      inactiveColor: Colors.white,
-                      activeIcon: Icon(
-                        Icons.nightlight_round,
-                        color: Color(0xFFF8E3A1),
-                      ),
-                      inactiveIcon: Icon(
-                        Icons.wb_sunny,
-                        color: Color(0xFFFFDF5D),
-                      ),
+                      height: 60.0,
+                      toggleSize: 52.6,
+                      borderRadius: 100.0,
+                      padding: 1.0,
+                      activeToggleColor: const Color(0xFF6E40C9),
+                      inactiveToggleColor: const Color(0xFFf5ec27),
+                      activeColor: const Color(0xFF271052),
+                      inactiveColor: const Color(0xFFbfe6f6),
+                      inactiveToggleBorder:  Border.all(color: Color(0xFFe4c741), width: 5),
+                      inactiveSwitchBorder: Border.all(color: Color(0xFF83c1d6), width: 5),
+                      activeIcon: constants.moon,
+                     // inactiveIcon: constants.sun,
                 value: status,
                 onToggle: (val) {
                   setState(() {
